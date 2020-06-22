3f7ba054c482e69ce005f3280041e86c84beed48
mohsinbohra <mohsinbohra20@gmail.com>
2020-06-22 10:15:44 +0530

    Exercise (Instructions): Presentational and Container Components

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import Main from './components/MainComponent/MainComponent';
+
+
+class App extends Component {
+
+   
+
+    render() {
+        return (
+            <div className='App'>
+               
+                <Main />
+            </div >
+
+        );
+    }
+}
+
+export default App;
---
+++ src/shared/dishes.js
@@ -0,0 +1,187 @@
+export const DISHES =
+    [
+        {
+            id: 0,
+            name: 'Uthappizza',
+            image: 'assets/images/uthappizza.png',
+            category: 'mains',
+            label: 'Hot',
+            price: '4.99',
+            description: 'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
+            comments: [
+                {
+                    id: 0,
+                    rating: 5,
+                    comment: "Imagine all the eatables, living in conFusion!",
+                    author: "John Lemon",
+                    date: "2019-10-16T17:57:28.556094Z"
+                },
+                {
+                    id: 1,
+                    rating: 4,
+                    comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
+                    author: "Paul McVites",
+                    date: "2019-09-05T17:57:28.556094Z"
+                },
+                {
+                    id: 2,
+                    rating: 3,
+                    comment: "Eat it, just eat it!",
+                    author: "Michael Jaikishan",
+                    date: "2017-02-13T17:57:28.556094Z"
+                },
+                {
+                    id: 3,
+                    rating: 4,
+                    comment: "Ultimate, Reaching for the stars!",
+                    author: "Ringo Starry",
+                    date: "2018-12-02T17:57:28.556094Z"
+                },
+                {
+                    id: 4,
+                    rating: 2,
+                    comment: "It's your birthday, we're gonna party!",
+                    author: "25 Cent",
+                    date: "2018-12-02T17:57:28.556094Z"
+                }
+            ]
+        },
+        {
+            id: 1,
+            name: 'Zucchipakoda',
+            image: 'assets/images/zucchipakoda.png',
+            category: 'appetizer',
+            label: '',
+            price: '1.99',
+            description: 'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce',
+            comments: [
+                {
+                    id: 0,
+                    rating: 5,
+                    comment: "Imagine all the eatables, living in conFusion!",
+                    author: "John Lemon",
+                    date: "2018-10-16T17:57:28.556094Z"
+                },
+                {
+                    id: 1,
+                    rating: 4,
+                    comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
+                    author: "Paul McVites",
+                    date: "2019-09-05T17:57:28.556094Z"
+                },
+                {
+                    id: 2,
+                    rating: 3,
+                    comment: "Eat it, just eat it!",
+                    author: "Michael Jaikishan",
+                    date: "2019-02-13T17:57:28.556094Z"
+                },
+                {
+                    id: 3,
+                    rating: 4,
+                    comment: "Ultimate, Reaching for the stars!",
+                    author: "Ringo Starry",
+                    date: "2019-12-02T17:57:28.556094Z"
+                },
+                {
+                    id: 4,
+                    rating: 2,
+                    comment: "It's your birthday, we're gonna party!",
+                    author: "25 Cent",
+                    date: "2017-12-02T17:57:28.556094Z"
+                }
+            ]
+        },
+        {
+            id: 2,
+            name: 'Vadonut',
+            image: 'assets/images/vadonut.png',
+            category: 'appetizer',
+            label: 'New',
+            price: '1.99',
+            description: 'A quintessential ConFusion experience, is it a vada or is it a donut?',
+            comments: [
+                {
+                    id: 0,
+                    rating: 5,
+                    comment: "Imagine all the eatables, living in conFusion!",
+                    author: "John Lemon",
+                    date: "2018-10-16T17:57:28.556094Z"
+                },
+                {
+                    id: 1,
+                    rating: 4,
+                    comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
+                    author: "Paul McVites",
+                    date: "2018-09-05T17:57:28.556094Z"
+                },
+                {
+                    id: 2,
+                    rating: 3,
+                    comment: "Eat it, just eat it!",
+                    author: "Michael Jaikishan",
+                    date: "2018-02-13T17:57:28.556094Z"
+                },
+                {
+                    id: 3,
+                    rating: 4,
+                    comment: "Ultimate, Reaching for the stars!",
+                    author: "Ringo Starry",
+                    date: "2018-12-02T17:57:28.556094Z"
+                },
+                {
+                    id: 4,
+                    rating: 2,
+                    comment: "It's your birthday, we're gonna party!",
+                    author: "25 Cent",
+                    date: "2020-02-02T17:57:28.556094Z"
+                }
+            ]
+        },
+        {
+            id: 3,
+            name: 'ElaiCheese Cake',
+            image: 'assets/images/elaicheesecake.png',
+            category: 'dessert',
+            label: '',
+            price: '2.99',
+            description: 'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms',
+            comments: [
+                {
+                    id: 0,
+                    rating: 5,
+                    comment: "Imagine all the eatables, living in conFusion!",
+                    author: "John Lemon",
+                    date: "2018-10-16T17:57:28.556094Z"
+                },
+                {
+                    id: 1,
+                    rating: 4,
+                    comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
+                    author: "Paul McVites",
+                    date: "2020-03-05T17:57:28.556094Z"
+                },
+                {
+                    id: 2,
+                    rating: 3,
+                    comment: "Eat it, just eat it!",
+                    author: "Michael Jaikishan",
+                    date: "2019-02-13T17:57:28.556094Z"
+                },
+                {
+                    id: 3,
+                    rating: 4,
+                    comment: "Ultimate, Reaching for the stars!",
+                    author: "Ringo Starry",
+                    date: "2018-12-02T17:57:28.556094Z"
+                },
+                {
+                    id: 4,
+                    rating: 2,
+                    comment: "It's your birthday, we're gonna party!",
+                    author: "25 Cent",
+                    date: "2019-12-02T17:57:28.556094Z"
+                }
+            ]
+        }
+    ];
